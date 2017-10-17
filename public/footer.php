f3567bb273f6f80a2d1b1ef9e4d393daeff9ebfe
kevinliposl <kevinliposl@gmail.com>
2017-10-16 22:47:17 -0600

    init

--- public/footer.php
+++ public/footer.php
@@ -31,47 +31,10 @@
 ============================================= -->
 <div id="gotoTop" class="icon-angle-up"></div>
 
-
 <!-- Footer Scripts
 ============================================= -->
 <script src="public/js/jquery.js" type="text/javascript"></script>
 <script src="public/js/plugins.js" type="text/javascript"></script>
-<script src="public/js/components/moment.js" type="text/javascript"></script>
-
-<!-- Charts JavaScripts
-============================================= -->
-<script src="public/js/Chart.bundle.min.js" type="text/javascript"></script>
-<script src="public/js/utils.js" type="text/javascript"></script>
-
-<!-- Bootstrap Select2
-============================================= -->
-<script src="public/js/select2.js" type="text/javascript"></script>
-
-<!-- Bootstrap Editable Plugin
-============================================= -->
-<script src="public/js/components/bs-editable.js" type="text/javascript"></script>
-
-<!-- Bootstrap Typeaheadjs Plugin SE QUITO ELEMENTOS
-============================================= -->
-<script src="public/js/typeahead.js" type="text/javascript"></script>
-
-<!-- Date & Time Picker JS
-============================================= -->
-<script src="public/js/datepicker.js" type="text/javascript"></script>
-<script src="public/js/components/timepicker.js" type="text/javascript"></script>
-
-<!-- Bootstrap Select Plugin
-============================================= -->
-<script src="public/js/components/bs-select.js" type="text/javascript"></script>
-
-<!-- Select Splitter Plugin
-============================================= -->
-<script src="public/js/components/selectsplitter.js" type="text/javascript"></script>
-
-<script type="text/javascript">
-    $('.selectsplitter').selectsplitter();
-</script>
-
 <script src="public/js/functions.js" type="text/javascript"></script>
 
 </body>
